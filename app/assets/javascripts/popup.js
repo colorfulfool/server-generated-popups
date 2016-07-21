c6ef570bc0f6af5f9b8bc5caf1aec7d39840f1d5
colorfulfool <t0ny.hamster@gmail.com>
2016-07-22 02:15:09 +0300

    Different method for `top` calculation

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -12,10 +12,11 @@ function PopupClass(html, options) {
     'margin-left': (options.width/2 * -1).toString() + 'px'
   })
   this.popupWindow.append(contents)
-
-  this.distanceFromTop = ($(window).height() - this.popupWindow.height())/2
 }
-  
+
+PopupClass.prototype.distanceFromTop = function () {
+  return ($(window).height() - this.popupWindow.height())/2
+}
 PopupClass.prototype.move = function (from, to, callback) {
   this.popupWindow.css('top', from)
   this.popupWindow.animate({top: to}, callback)
@@ -24,12 +25,12 @@ PopupClass.prototype.move = function (from, to, callback) {
 PopupClass.prototype.show = function (direction, callback) {
   $('body').append(this.popupWindow)
   startFrom = (direction == 'up') ? '180%' : '-80%'
-  this.move(startFrom, this.distanceFromTop, callback)
+  this.move(startFrom, this.distanceFromTop(), callback)
   return this;
 }
 PopupClass.prototype.hide = function (direction) {
   moveTo = (direction == 'down') ? '180%' : '-80%'
-  this.move(this.distanceFromTop, moveTo, function () {
+  this.move(this.distanceFromTop(), moveTo, function () {
     this.remove() // this == $('.popup') here
   })
 }
